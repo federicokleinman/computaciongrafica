7cc6a84cdbaac0ab0df383ddd281095d2fe35cea
Fernando Torterolo <fernando.torterolo@autodesk.com>
2015-10-21 09:34:46 -0300

    implementacion parte2

---
+++ obligatorio/2/main.c
@@ -0,0 +1,113 @@
+#include "../framework/canvas.h"
+#include "../framework/mm.h"
+#include "../Obj/Obj.h"
+#include "GL/gl.h"
+
+/**
+ * 1. Rasterización de modelos OBJ, dibujando triángulos sólidos o una “malla de alambre”.
+ */
+int main(int argc, char* argv[])
+{
+	// Crear una ventana de 750x750 pixels.
+	int cw = 750;
+	int ch = 750;
+
+	cg_init(cw, ch, NULL);
+
+	// Actualizar la pantalla:
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glMatrixMode(GL_PROJECTION);
+	glViewport(0,0,cw, ch);
+	glFrustum(-1,1,-1,1,1,1000);
+
+	Obj* obj = obj_load("../Models/knight.obj");
+
+	float ang = 0.0f;
+	float pitch = 0.0f;
+	float ang_vel = 1.0f;
+
+	char done = 0;
+	char wireframe = 0;
+	char bfc = 0;
+	glEnable(GL_DEPTH_TEST);
+	char zbuff = 1;
+	unsigned char key_pressed[1024];
+	memset(key_pressed, 0, 1024);
+
+	while (!done)
+	{
+		SDL_Event event;
+		while(SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				case SDL_KEYDOWN:
+					key_pressed[event.key.keysym.sym] = 1;
+
+					if (event.key.keysym.sym == SDLK_z)
+					{
+						zbuff = !zbuff;
+						if(zbuff)
+							glEnable(GL_DEPTH_TEST);
+						else
+							glDisable(GL_DEPTH_TEST);
+						break;
+					}
+					else if (event.key.keysym.sym == SDLK_b)
+					{
+						bfc = !bfc;
+						if(bfc)
+						{
+							glEnable(GL_CULL_FACE);
+							glCullFace(GL_BACK);
+							glFrontFace(GL_CW);
+						}
+						else
+							glDisable(GL_CULL_FACE);
+						break;
+					}
+					else if (event.key.keysym.sym == SDLK_m)
+					{
+						wireframe = !wireframe;
+						if(wireframe)
+							glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+						else
+							glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+						break;
+					}
+					else if (event.key.keysym.sym != SDLK_ESCAPE)
+						break;
+				case SDL_QUIT :
+					done = 1;
+					break;
+				case SDL_KEYUP:
+					key_pressed[event.key.keysym.sym] = 0;
+
+			}
+		}
+
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslatef(0.0f, 0.0f, -50.0f);
+		glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+		glRotatef(ang, 0.0f, 1.0f, 0.0f);
+		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+
+		if(key_pressed[SDLK_RIGHT]) ang += ang_vel;
+		if(key_pressed[SDLK_LEFT]) ang -= ang_vel;
+		if(key_pressed[SDLK_UP]) pitch += ang_vel;
+		if(key_pressed[SDLK_DOWN]) pitch -= ang_vel;
+
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		obj_render(obj);
+		cg_repaint();
+	}
+
+	// Liberar recursos:
+	obj_free(obj);
+	cg_close();
+
+	return 0;
+}
---
+++ obligatorio/2/readme.md
@@ -0,0 +1,33 @@
+### Computación Gráfica
+
+# Parte 2 Obligatorio.
+
+Determinación de superficies ocultas por medio de Z-Buffer y optimización por Backface culling.
+
+
+# Ejecucion.
+
+Correr el script run.sh
+
+
+| Tecla | Descripcion                          |
+|-------|--------------------------------------|
+| M     | Activa imagen Solida / Malla Alambre |
+| Z     | Activa / Desactiva GL_DEPTH_TEST     |
+| B     | Activa / Desactiva GL_CULL_FACE      |
+| RIGHT | Rota a la derecha horizontalmente    |
+| LEFT  | Rota a la izquierda horizontalmente  |
+| UP    | Rota hacia arriba verticalmente      |
+| DOWN  | Rota hacia abajo verticalmente       |
+
+# Resultadoss
+
+**Sin BackFace Culling**
+
+![alt text](https://github.com/nandotorterolo/computaciongrafica/blob/master/obligatorio/2/SinBackFaceCulling.png "SinBackFaceCulling")
+
+**Con BackFace Culling**
+
+![alt text](https://github.com/nandotorterolo/computaciongrafica/blob/master/obligatorio/2/ConBackFaceCulling.png "ConBackFaceCulling")
+
+
