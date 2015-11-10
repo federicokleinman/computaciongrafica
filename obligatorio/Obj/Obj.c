52ddbfa387836c25f34aa47c625c43124529b2af
Fernando Torterolo <fernando.torterolo@autodesk.com>
2015-11-10 17:47:30 -0300

    new render

--- obligatorio/Obj/Obj.c
+++ obligatorio/Obj/Obj.c
@@ -9,6 +9,9 @@
 FILE *stream;
 Obj *gameobject[MAX_GAME_OBJECTS];
 int numobjects = 0,objectsInFile = 0;
+float * vertices;
+float * normales;
+float * texturas;
 
 void obj_CountObjects()
 {
@@ -119,6 +122,51 @@ void LoadFaces(int theobject)
     }
 }
 
+void LoadFaces2(int theobject)
+{
+    char s[100];
+    int numsides,vert[4], norm[4], text[4];
+    char *tok;
+    char temp[100];
+    int i,facecount = 0;
+    FACE2 *newface;
+
+    if(ObjFileQue(theobject,'f')){
+        while ((fgets(s,100,stream)) != NULL && s[0] == 'f')
+        {
+            numsides = 4;
+            tok = strtok(s," ");
+            i = 0;
+            while ((tok = strtok(NULL," ")) != NULL)
+            {
+                strcpy(temp,tok);
+                temp[strcspn(temp,"/")] = 0;
+                vert[i] = (int)strtol(temp,NULL,10);
+
+                strcpy(temp,tok);
+                temp[strcspn(temp,"/")] = 0;
+                norm[i] = (int)strtol(temp,NULL,10);
+
+                strcpy(temp,tok);
+                temp[strcspn(temp,"/")] = 0;
+                text[i] = (int)strtol(temp,NULL,10);
+
+                i++;
+            }
+            newface = (FACE2 *)malloc(sizeof(FACE2));
+            gameobject[theobject + (numobjects - objectsInFile)]->faces2[facecount] = newface;
+            //gameobject[theobject + (numobjects - objectsInFile)]->faces2[facecount]->numsides = numsides;
+            for(i=0;i<3;i++) {
+                gameobject[theobject + (numobjects - objectsInFile)]->faces2[facecount]->vertices[i] = vert[i];
+                gameobject[theobject + (numobjects - objectsInFile)]->faces2[facecount]->normales[i] = norm[i];
+                gameobject[theobject + (numobjects - objectsInFile)]->faces2[facecount]->texturas[i] = text[i];
+            }
+            facecount++;
+        }
+        gameobject[theobject + (numobjects - objectsInFile)]->numfaces2 = facecount;
+    }
+}
+
 void LoadVertices(int theobject)
 {
     char s[100];
@@ -150,6 +198,68 @@ void LoadVertices(int theobject)
     }
 }
 
+void LoadNormales(int theobject)
+{
+    char s[100];
+    char *tok;
+    char temp[100];
+    float coor[3];
+    int normalCount = 0,i;
+    VERTEX *newvert;
+
+    if (ObjFileQue(theobject,'n')){
+        while ((fgets(s,100,stream)) != NULL && (s[0] == 'n' && s[1] == ' '))
+        {
+            tok = strtok(s," ");
+            for (i=0;i<3;i++)
+            {
+                tok = strtok(NULL," ");
+                strcpy(temp,tok);
+                temp[strcspn(temp," ")] = 0;
+                coor[i] = (float)atof(temp);
+            }
+            newvert = (VERTEX *)malloc(sizeof(VERTEX));
+            gameobject[theobject + (numobjects - objectsInFile)]->normales[normalCount] = newvert;
+            gameobject[theobject + (numobjects - objectsInFile)]->normales[normalCount]->x = coor[0];
+            gameobject[theobject + (numobjects - objectsInFile)]->normales[normalCount]->y = coor[1];
+            gameobject[theobject + (numobjects - objectsInFile)]->normales[normalCount]->z = coor[2];
+            normalCount++;
+        }
+        gameobject[theobject + (numobjects - objectsInFile)]->numNormales = normalCount;
+    }
+}
+
+void LoadTexturas(int theobject)
+{
+    char s[100];
+    char *tok;
+    char temp[100];
+    float coor[2];
+    int textCount = 0,i;
+    VERTEX2 *newvert;
+
+    if (ObjFileQue(theobject,'t')){
+        while ((fgets(s,100,stream)) != NULL && (s[0] == 't' && s[1] == ' '))
+        {
+            tok = strtok(s," ");
+            for (i=0;i<2;i++)
+            {
+                tok = strtok(NULL," ");
+                strcpy(temp,tok);
+                temp[strcspn(temp," ")] = 0;
+                coor[i] = (float)atof(temp);
+            }
+            newvert = (VERTEX2 *)malloc(sizeof(VERTEX2));
+            gameobject[theobject + (numobjects - objectsInFile)]->texturas[textCount] = newvert;
+            gameobject[theobject + (numobjects - objectsInFile)]->texturas[textCount]->x = coor[0];
+            gameobject[theobject + (numobjects - objectsInFile)]->texturas[textCount]->y = coor[1];
+            textCount++;
+        }
+        gameobject[theobject + (numobjects - objectsInFile)]->numTexturas = textCount;
+    }
+}
+
+
 Obj* obj_load(char *filename)
 {
     // open and read obj file
@@ -159,12 +269,17 @@ Obj* obj_load(char *filename)
     if((stream = fopen( filename, "r+t" )) != NULL )
     {
         obj_CountObjects();
-        for (i=numobjects - objectsInFile;i<numobjects;i++)
+        for (i=numobjects - objectsInFile; i<numobjects; i++)
         {
             newobject = (Obj *)malloc(sizeof(Obj));
             gameobject[i] = newobject;
-            LoadFaces(i - (numobjects - objectsInFile));
             LoadVertices(i - (numobjects - objectsInFile));
+            LoadNormales(i - (numobjects - objectsInFile));
+            LoadTexturas(i - (numobjects - objectsInFile));
+            LoadFaces2(i - (numobjects - objectsInFile));
+            creteArrayVertices(gameobject[i]);
+            creteArrayNormales(gameobject[i]);
+            creteArrayTexturas(gameobject[i]);
         }
         fclose( stream );
     }
@@ -177,32 +292,140 @@ void obj_render(Obj * obj)
     glBegin(GL_TRIANGLES);
     for(i=0; i< obj->numfaces; i++) {
         glVertex3f(
-                obj->vertices[obj->faces[i]->vert[0] -1]->x,
-                obj->vertices[obj->faces[i]->vert[0] -1]->y,
-                obj->vertices[obj->faces[i]->vert[0] -1]->z
+            obj->vertices[obj->faces[i]->vert[0] -1]->x,
+            obj->vertices[obj->faces[i]->vert[0] -1]->y,
+            obj->vertices[obj->faces[i]->vert[0] -1]->z
         );
         glVertex3f(
-                obj->vertices[obj->faces[i]->vert[1] -1]->x,
-                obj->vertices[obj->faces[i]->vert[1] -1]->y,
-                obj->vertices[obj->faces[i]->vert[1] -1]->z
+            obj->vertices[obj->faces[i]->vert[1] -1]->x,
+            obj->vertices[obj->faces[i]->vert[1] -1]->y,
+            obj->vertices[obj->faces[i]->vert[1] -1]->z
         );
         glVertex3f(
-                obj->vertices[obj->faces[i]->vert[2] -1]->x,
-                obj->vertices[obj->faces[i]->vert[2] -1]->y,
-                obj->vertices[obj->faces[i]->vert[2] -1]->z
+            obj->vertices[obj->faces[i]->vert[2] -1]->x,
+            obj->vertices[obj->faces[i]->vert[2] -1]->y,
+            obj->vertices[obj->faces[i]->vert[2] -1]->z
         );
     }
     glEnd();
 }
 
+void creteArrayVertices(Obj * obj)
+{
+    printf("numero de vertices %d \n", obj->numverts);
+    printf("numero de caras faces2 %d \n", obj->numfaces2);
+    int i;
+    int verticesArraySize = obj->numfaces2*3*3;
+    vertices = (float *) malloc(sizeof(float) * verticesArraySize);
+
+    for (i = 0; i < obj->numfaces2 ; ++i) {
+        vertices[9*i + 0] = obj->vertices[obj->faces2[i]->vertices[0] -1 ]->x;
+        vertices[9*i + 1] = obj->vertices[obj->faces2[i]->vertices[0] -1 ]->y;
+        vertices[9*i + 2] = obj->vertices[obj->faces2[i]->vertices[0] -1 ]->z;
+
+        vertices[9*i + 3] = obj->vertices[obj->faces2[i]->vertices[1] -1 ]->x;
+        vertices[9*i + 4] = obj->vertices[obj->faces2[i]->vertices[1] -1 ]->y;
+        vertices[9*i + 5] = obj->vertices[obj->faces2[i]->vertices[1] -1 ]->z;
+
+        vertices[9*i + 6] = obj->vertices[obj->faces2[i]->vertices[2] -1 ]->x;
+        vertices[9*i + 7] = obj->vertices[obj->faces2[i]->vertices[2] -1 ]->y;
+        vertices[9*i + 8] = obj->vertices[obj->faces2[i]->vertices[2] -1 ]->z;
+    }
+}
+
+void creteArrayNormales(Obj * obj)
+{
+    printf("numero de normales %d \n", obj->numNormales);
+    printf("numero de caras faces2 %d \n", obj->numfaces2);
+    int i;
+    int normalesArraySize = obj->numfaces2*3*3;  //5706
+    normales = (float *) malloc(sizeof(float) * normalesArraySize);
+
+    for (i = 0; i < obj->numfaces2 ; ++i) {
+        normales[9*i + 0] = obj->normales[obj->faces2[i]->normales[0] -1 ]->x;
+        normales[9*i + 1] = obj->normales[obj->faces2[i]->normales[0] -1 ]->y;
+        normales[9*i + 2] = obj->normales[obj->faces2[i]->normales[0] -1 ]->z;
+
+        normales[9*i + 3] = obj->normales[obj->faces2[i]->normales[1] -1 ]->x;
+        normales[9*i + 4] = obj->normales[obj->faces2[i]->normales[1] -1 ]->y;
+        normales[9*i + 5] = obj->normales[obj->faces2[i]->normales[1] -1 ]->z;
+
+        normales[9*i + 6] = obj->normales[obj->faces2[i]->normales[2] -1 ]->x;
+        normales[9*i + 7] = obj->normales[obj->faces2[i]->normales[2] -1 ]->y;
+        normales[9*i + 8] = obj->normales[obj->faces2[i]->normales[2] -1 ]->z; //5705
+    }
+}
+
+void creteArrayTexturas(Obj * obj)
+{
+    printf("numero de texturas %d \n", obj->numTexturas);
+    printf("numero de caras faces2 %d \n", obj->numfaces2); // 634
+    int i;
+    int texturasArraySize = obj->numfaces2*3*2;
+    printf("numero texturasArraySize %d \n", texturasArraySize); //634*3*2 = 3804
+    texturas = (float *) malloc(sizeof(float) * texturasArraySize);
+
+    for (i = 0; i < obj->numfaces2; ++i) {
+        if(i<500) {
+            texturas[6 * i + 0] = obj->texturas[obj->faces2[i]->texturas[0] - 1]->x;
+            texturas[6 * i + 1] = obj->texturas[obj->faces2[i]->texturas[0] - 1]->y;
+
+            texturas[6 * i + 2] = obj->texturas[obj->faces2[i]->texturas[1] - 1]->x;
+            texturas[6 * i + 3] = obj->texturas[obj->faces2[i]->texturas[1] - 1]->y;
+
+            texturas[6 * i + 4] = obj->texturas[obj->faces2[i]->texturas[2] - 1]->x;
+            texturas[6 * i + 5] = obj->texturas[obj->faces2[i]->texturas[2] - 1]->y;  //3803
+        }
+    }
+}
+
+void obj_render2(Obj * obj)
+{
+    // activate and specify pointer to vertex array
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+
+    glEnableClientState(GL_NORMAL_ARRAY);
+    glNormalPointer(GL_FLOAT,0, normales);
+
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glTexCoordPointer(2, GL_FLOAT, 0, texturas);
+
+    // draw
+//    glDrawArrays(GL_TRIANGLES, 0, 1902);
+    glDrawArrays(GL_TRIANGLES, 0, 4096);
+
+    // deactivate vertex arrays after drawing
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+
+
 void obj_free(Obj * obj) {
     int i;
-    for (i=0; i < obj->numfaces; ++i) {
-        free(obj->faces[i]);
+
+    for (i = 0; i < obj->numfaces2; ++i) {
+        free(obj->faces2[i]);
+    }
+
+    for (i=0; i < MAX_FACES; ++i) {
+        //if (obj->faces[i] != NULL)
+            //free(obj->faces[i]);
     }
 
     for (i = 0; i < obj->numverts; ++i) {
-        free(obj->vertices[i]);
+ //       free(obj->vertices[i]);
     }
+
+    for (i = 0; i < obj->numTexturas; ++i) {
+   //     free(obj->texturas[i]);
+    }
+
+    free(vertices);
+    free(normales);
+    free(texturas);
+
     free(obj);
+
 }
